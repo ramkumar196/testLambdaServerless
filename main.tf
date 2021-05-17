f7a8fb36f1377f42cfdc28e6b598babdb907b13a
ramkumar196 <ram196kumar@gmail.com>
2021-05-17 11:22:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,38 +1,9 @@
-
-resource "aws_s3_bucket" "bucket" {
-  bucket        = "some-bucket"
-}
-resource "aws_sns_topic" "topic" {
-  name = "ramTerraformTopic"
-
-  policy = <<POLICY
-  {
-      "Version":"2012-10-17",
-      "Statement":[{
-          "Effect": "Allow",
-          "Principal": {"Service":"s3.amazonaws.com"},
-          "Action": "SNS:Publish",
-          "Resource":  "arn:aws:sns:${var.region}:${var.account_id}:ramTerraformTopic",
-          "Condition":{
-              "ArnLike":{"aws:SourceArn":"${aws_s3_bucket.bucket.arn}"}
-          }
-      }]
-  }
-  POLICY
-}
 provider "aws" {
   region = var.region
 }
 
-data archive_file lambda {
-  type        = "zip"
-  source_file = "index.js"
-  output_path = "lambda_function.zip"
-}
-
-
-resource aws_iam_role iam {
-  name = "iam_for_lambda_tf"
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
 
   assume_role_policy = <<EOF
 {
@@ -51,56 +22,22 @@ resource aws_iam_role iam {
 EOF
 }
 
-resource aws_iam_policy this {
-  name        = "ramTerraformLambdaPolicy"
-  description = "Allow to access base resources and trigger transcoder"
-  policy      = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "SomeVeryDefaultAndOpenActions",
-            "Effect": "Allow",
-            "Action": [
-                "logs:CreateLogGroup",
-                "ec2:DescribeNetworkInterfaces",
-                "ec2:CreateNetworkInterface",
-                "ec2:DeleteNetworkInterface",
-                "logs:CreateLogStream",
-                "logs:PutLogEvents"
-            ],
-            "Resource": [
-                "*"
-            ]
-        }
-    ]
-}
-EOF
-}
-module lambda {
-    source = "terraform-aws-modules/lambda/aws"
+resource "aws_lambda_function" "test_lambda" {
+  filename      = "lambda_function_payload.zip"
+  function_name = "lambda_function_name"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "exports.test"
 
-  function_name  = "ramTerraformLambda"
-  filename       = data.archive_file.lambda.output_path
-  description    = "description should be here"
-  handler        = "index.handler"
-  runtime        = "nodejs12.x"
-  memory_size    = "128"
-  concurrency    = "5"
-  lambda_timeout = "20"
-  log_retention  = "1"
-  role_arn       = aws_iam_role.iam.arn
+  # The filebase64sha256() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
+  # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
+  source_code_hash = filebase64sha256("lambda_function_payload.zip")
 
-  vpc_config = {
-    subnet_ids         = ["sb-q53asdfasdfasdf", "sf-3asdfasdfasdf6"]
-    security_group_ids = ["sg-3asdfadsfasdfas"]
-  }
+  runtime = "nodejs12.x"
 
-  environment = {
-    Environment = "test"
-  }
-
-  tags = {
-    Environment = "test"
+  environment {
+    variables = {
+      foo = "bar"
+    }
   }
 }
